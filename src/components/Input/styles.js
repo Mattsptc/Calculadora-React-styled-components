e543ac96e5d543e12e6dab1e962af29ceef512d7
Mattsptc <mateusptc@gmail.com>
2022-11-18 18:09:18 -0300

    desafio resolvido

---
+++ src/components/Input/styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAAAFF;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    font-size: 24px;
+    font-family: 'Roboto';
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: white;
+        border: 0;
+        font-size: 24px;
+        padding: 0 10px;
+        display: flex;
+        float: right;
+        flex-direction: column;
+        align-items: flex-end;
+        color: black;
+    }
+
+`
+
